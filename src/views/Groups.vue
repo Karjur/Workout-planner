f0d48b0d89d0f40becd8febfdcd5e0ba814e3b52
Polina Volohhonskaja <povolo@ttu.ee>
2022-10-20 20:46:04 +0300

    gruppi lisamise nuppu lisamine

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -1,7 +1,11 @@
 <template>
-    <GroupList title="Group list" />
-  </template>
+  <div class="text-right mb-3">    
+<a class="btn btn-primary" align-right href = "newgroup" :to="{name: 'Lisa grupp'}" role="button">Lisa grupp</a>
+</div>
+<GroupList title="Gruppide nimekiri" />
+</template>
   
   <script setup lang="ts">
   import GroupList from '@/components/GroupList.vue';
   </script>
+ 
