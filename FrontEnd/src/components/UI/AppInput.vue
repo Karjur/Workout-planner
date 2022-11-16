a8f427a9c3737112465876dfee2d3978ee0d1491
Polina Volohhonskaja <povolo@ttu.ee>
2022-11-16 23:30:58 +0200

    appInput korras

--- FrontEnd/src/components/UI/AppInput.vue
+++ FrontEnd/src/components/UI/AppInput.vue
@@ -5,14 +5,14 @@
         </div>
         <div class="col-auto" v-if="selectList && selectLabel">
             <!-- {{modelValue}} -->
-            <select class="form-select" :value="modelValue"  @change="inputAction($event.target?.value)">
+            <select class="form-select" :value="modelValue"  @change="inputAction((<any>$event).target?.value)">
                 <option>{{selectLabel}}</option>
                 <option v-for="item in selectList" :key="item.key" :value="item.value">{{item.key}}</option>
 
               </select>
         </div>
         <div class="col-auto" v-else>
-            <input :type="typeof(modelValue) == 'string' ? 'text' : 'number'" :value="modelValue" class="form-control" @input="inputAction($event.target?.value)">
+            <input :type="typeof(modelValue) == 'string' ? 'text' : 'number'" :value="modelValue" class="form-control" @input="inputAction((<any>$event).target?.value)">
         </div>
     </div>
 </template>
